30dd8af8e1b06351313cea1c3057bbd3580b7ba4
Alexander <sany199574@gmail.com>
2026-05-15 15:30:50 +0300

    Реализовал слой  - Domain

---
+++ src/Backend/PS.StringOpsService/StringOpsService/Domain/PS.StringOpsService.Domain/Contexts/ProcessContext.cs
@@ -0,0 +1,8 @@
+﻿namespace PS.StringOpsService.Domain.Contexts
+{
+    public record ProcessContext(
+         string Value,
+         IReadOnlyDictionary<string, object>? Items = null
+         );
+
+}
---
+++ src/Backend/PS.StringOpsService/StringOpsService/Domain/PS.StringOpsService.Domain/Operations/Interfaces/IStringOperation.cs
@@ -0,0 +1,9 @@
+﻿using PS.StringOpsService.Domain.Contexts;
+
+namespace PS.StringOpsService.Domain.Operations.Interfaces
+{
+    public interface IStringOperation
+    {
+        public ProcessContext Apply(ProcessContext context);
+    }
+}
